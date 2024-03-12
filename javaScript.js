82192eca1f789ff78717bdf5da75b75092f822ec
Veljko03 <veljkodovragovic@gmail.com>
2024-03-12 22:22:38 +0100

    made constructor for book and function that tores new book into array

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,6 @@
+
+const books = document.querySelector(".currBooks");
+
 const myLibrary = [];
 
 function Book(title,author,pages, read){
@@ -10,5 +13,16 @@ function Book(title,author,pages, read){
 }
 
 function addBookToLibrary() {
-
+  const newBook= new Book();
+  let titleInput = prompt("enter title of the book");
+  newBook.title = titleInput;
+  let authorInput = prompt("Enter autor name and surname");
+  newBook.author = authorInput;
+  let pagesInput = prompt("Enter num of pages");
+  newBook.pages = parseInt(pagesInput);
+  myLibrary.push(newBook);
+    
+    
 }
+addBookToLibrary();
+console.log(myLibrary);
