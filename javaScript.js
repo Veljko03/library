cd6aec4edbb29da2019e55bbe1a3bf9cdf77e0ff
Veljko03 <veljkodovragovic@gmail.com>
2024-03-13 11:19:04 +0100

    diplayBooks funtion made

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,6 @@
 
 const books = document.querySelector(".currBooks");
+const btnAdd = document.querySelector(".add");
 
 const myLibrary = [];
 
@@ -16,13 +17,54 @@ function addBookToLibrary() {
   const newBook= new Book();
   let titleInput = prompt("enter title of the book");
   newBook.title = titleInput;
+
+
+
   let authorInput = prompt("Enter autor name and surname");
   newBook.author = authorInput;
+  
+
+
   let pagesInput = prompt("Enter num of pages");
   newBook.pages = parseInt(pagesInput);
+
+
+
   myLibrary.push(newBook);
     
     
 }
-addBookToLibrary();
+
 console.log(myLibrary);
+
+
+btnAdd.addEventListener("click", ()=>{
+    addBookToLibrary();
+    displayBooks();
+})
+
+
+function displayBooks(){
+    
+        myLibrary.forEach(book => {
+           
+                const card = document.createElement("div");
+                card.classList.add("card");
+                const titleDisplay= document.createElement("h1");
+                titleDisplay.innerText ="Book title: "+ book.title;
+                card.appendChild(titleDisplay);
+
+                const authorDisplay = document.createElement("h4");
+                authorDisplay.textContent ="Author: "+ book.author;
+                card.appendChild(authorDisplay);
+
+                const pagesDisplay = document.createElement("h4");
+                pagesDisplay.textContent ="Number of pages: "+ book.pages;
+                card.appendChild(pagesDisplay);
+
+                books.appendChild(card);
+            
+        });
+        
+    
+}
